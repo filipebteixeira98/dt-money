a3250c713a698af4c3e4e378f3c9c33e9141d849
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2021-05-06 18:31:56 -0300

    feat: create dashboard & summary components

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,33 @@
+import { Container } from './styles';
+
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import totalImg from '../../assets/total.svg';
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Incomes</p>
+          <img src={incomeImg} alt="Incomes" />
+        </header>
+        <strong>$1000.00</strong>
+      </div>
+      <div>
+        <header>
+          <p>Outcomes</p>
+          <img src={outcomeImg} alt="Outcomes" />
+        </header>
+        <strong>- $312.00</strong>
+      </div>
+      <div className="highlight-background">
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="Total" />
+        </header>
+        <strong>$788.00</strong>
+      </div>
+    </Container>
+  );
+}
