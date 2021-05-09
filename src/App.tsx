6bfe9e6ff3f8083b3cc8a99b9f37bf3119c1e590
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2021-05-09 19:35:10 -0300

    refactor: change transactions context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import Modal from 'react-modal';
 
-import { TransactionsContext } from './TransactionsContext';
+import { TransactionsProvider } from './TransactionsContext';
 
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
@@ -25,7 +25,7 @@ export function App() {
   }
 
   return (
-    <TransactionsContext.Provider value={[]}>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <NewTransactionModal
@@ -33,6 +33,6 @@ export function App() {
         onRequestClose={handleCloseNewTransactionModal}
       />
       <GlobalStyle />
-    </TransactionsContext.Provider>
+    </TransactionsProvider>
   );
 }
